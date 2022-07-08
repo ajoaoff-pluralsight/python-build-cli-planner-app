178fd261402fff55d958735ddf4e25366696e122
Antonio Francisco <ajoaoff+pluralsight@gmail.com>
2022-07-08 17:54:54 -0300

    Added missing file

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,45 @@
+from abc import ABC, ABCMeta, abstractmethod
+from datetime import datetime
+from typing import Iterable
+
+from dateutil.parser import parse
+
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DeadlinedReminder(Iterable, ABC):
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if cls is not DeadlinedReminder:
+            return NotImplemented
+
+        def attr_in_hierarchy(attr):
+            return any (attr in SuperClass.__dict__ for SuperClass in subclass.__mro__)
+
+        if not all(attr_in_hierarchy(attr) for attr in ('__iter__', 'is_due')):
+            return NotImplemented
+
+        return True
+        
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DateReminder(DeadlinedReminder):
+
+    def __init__(self, text, date):
+        self.text = text
+        self.date = parse(date, dayfirst=True)
+
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
+
+    def is_due(self):
+        return self.date <= datetime.now()
